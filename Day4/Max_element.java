7098e604680eaaafac8b90bd9a00595c3cc3e573
Likitha123-lily <mlikitha126@gmail.com>
2025-08-01 14:12:04 +0530

    Day4

---
+++ Day4/Max_element.java
@@ -0,0 +1,29 @@
+package day3;
+
+import java.util.*;
+
+public class Max_element {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array :");
+		int n = sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("enter the elements in the array:");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int max=arr[0];
+		for(int i=0;i<arr.length;i++) {
+
+			if(max<arr[i]) {
+				max=arr[i];
+			}
+				
+			}
+		System.out.println(max);
+		}
+		
+	}
+
+
---
+++ Day4/Sum_array.java
@@ -0,0 +1,24 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Sum_array {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of the array :");
+		int n = sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("enter the elements in the array:");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int sum=0;
+		for(int i=0;i<n;i++) {
+			sum+=arr[i];
+		}
+		System.out.println(sum);
+
+	}
+
+}
